6f7bb19899426c7dba31e4c2d9255e544323ff26
baptiste <etud@port-dell-latitude-3500-17>
2022-04-11 13:43:14 +0200

    test texture

--- Projet/src/rendu/rendu.cpp
+++ Projet/src/rendu/rendu.cpp
@@ -1,8 +1,20 @@
 #include "rendu.h"
-#include "iostream"
+
+
+void renduAsteroide(Asteroide * a){
+     glPushMatrix();
+   {
+    glColor3f(0.5,0.5,0.5);
+    glTranslatef(a->posX(),a->posY(),-a->posZ());    
+    glutSolidSphere(a->getRad(),a->getSli(),a->getSta());
+    
+   }
+   glPopMatrix();
+}
 
 void renduVaisseau(Vaisseau * vaisseau){
     glPushMatrix();
+        glColor3f(1, 1, 1);
         glTranslatef(vaisseau->posx(), vaisseau->posy(), vaisseau->posz());
         glRotatef(180 + vaisseau->getAngle(), 0, 1, 0);
         glutSolidCone(1, 5, 30, 30);
@@ -12,3 +24,5 @@ void renduVaisseau(Vaisseau * vaisseau){
 void renduCamera(Vaisseau * vaisseau){
     gluLookAt(vaisseau->camera->posx(), vaisseau->camera->posy(), vaisseau->camera->posz(), vaisseau->posx(), vaisseau->posy(), vaisseau->posz(), 0, 1, 0);
 }
+
+
--- Projet/src/rendu/rendu.h
+++ Projet/src/rendu/rendu.h
@@ -1,5 +1,8 @@
 #include "../objets/vaisseau.h"
 #include "../objets/camera.h"
+#include "../objets/asteroide.h"
 
+
+void renduAsteroide(Asteroide * a);
 void renduVaisseau(Vaisseau * v);
 void renduCamera(Vaisseau * v);
