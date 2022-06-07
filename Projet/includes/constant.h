25c29bd4885d780b4e118311118dda1a85f2b909
lolux76 <lolux76@gmail.com>
2022-06-07 23:30:12 +0200

    objects constants

--- Projet/includes/constant.h
+++ Projet/includes/constant.h
@@ -1,8 +1,11 @@
 #include <GL/gl.h>
+#include "math.h"
 
 namespace gameconf
 {
     const unsigned int INVINCIBILITY_TIME = 3000;
+
+    const GLfloat DEG2RAD = M_PI / 180;
 
     // GAME CONFIG
     const unsigned int BASE_SCORE = 0;
@@ -14,6 +17,7 @@ namespace gameconf
     const unsigned int ASTEROID_OUT_OF_AREA_RELOCATE = 400;
     const unsigned int BIG_ASTEROID_SCORE = 20;
     const unsigned int MEDIUM_ASTEROID_SCORE = 50;
+    const unsigned int SMALL_ASTEROID_SCORE = 100;
 
     // ASTEROIDS STATS
     const int ASTEROID_MIN_RAND_POS = -199;
@@ -26,6 +30,7 @@ namespace gameconf
     const unsigned int ASTEROID_BASE_HITBOX = 0;
     const unsigned int ASTEROID_MOVE_FORWARD_ANGLE = 180;
     const bool ASTEROID_BASE_TOUCH = false;
+    const unsigned int ASTEROID_CONTACT_DAMAGE = 10;
 
     // BIG ASTEROID STATS
     const unsigned int BIG_ASTEROID_BASE_SIZE = 3;
@@ -41,6 +46,10 @@ namespace gameconf
     const unsigned int MEDIUM_ASTEROID_MIN_ANGLE = 1;
     const unsigned int MEDIUM_ASTEROID_MAX_ANGLE = 360;
 
+    // SMALL ASTEROID STATS
+    const unsigned int SMALL_ASTEROID_BASE_SIZE = 1;
+    const unsigned int SMALL_ASTEROID_BASE_HITBOX = 5;
+
     // ENNEMY STATS
     const int ENEMY_SPAWN_DELAY = 80000;
     const unsigned int ENEMY_CONTACT_DAMAGE = 30;
@@ -48,10 +57,32 @@ namespace gameconf
     const unsigned int ENEMY_MIN_LIFE = 0;
     const int ENEMY_MIN_RAND_POS = -100;
     const unsigned int ENEMY_MAX_RAND_POS = 100;
+    const unsigned int ENEMY_SPEED = 1;
+    const unsigned int ENEMY_PITCH = 2;
+    const unsigned int ENEMY_YAW = 0;
+    const unsigned int ENEMY_HITBOX = 7;
+    const unsigned int ENEMY_ROTATE = 180;
+    const unsigned int ENEMY_LIFE_LOST = 10;
+    const unsigned int ENEMY_DAMAGE = 10;
 
     // PLAYER STATS
     const unsigned int PLAYER_MIN_LIFE = 30;
     const unsigned int PLAYER_DEAD = 0;
+    const unsigned int PLAYER_BASE_POSITION = 0;
+    const unsigned int PLAYER_BASE_ROTATION = 0;
+    const unsigned int PLAYER_BASE_SPEED = 0;
+    const unsigned int PLAYER_LENGTH = 3;
+    const unsigned int PLAYER_WIDTH = 3;
+    const unsigned int PLAYER_HEIGHT = 3;
+    const unsigned int PLAYER_BASE_LIFE = 100;
+    const bool PLAYER_SPAWNING_INVINCIBILITY = false;
+    const unsigned int PLAYER_OUT_OF_ARENA_RELOCATION = 400;
+    const unsigned int BULLET_OUT_OF_ARENA_RELOCATION = 400;
+    const unsigned int PLAYER_PITCH_LIMIT = 60;
+
+    // CAMERA CONFIG
+    const unsigned int CAMERA_Y = 10;
+    const unsigned int CAMERA_Z = 20;
 
 };
 
@@ -59,4 +90,6 @@ namespace playercontrols
 {
     const int NEGATIVE_MOVEMENT = -2;
     const unsigned int POSITIVE_MOVEMENT = 2;
+    const GLfloat POSITIVE_SPEED_DECREASE = 0.96;
+    const GLfloat NEGATIVE_SPEED_DECREASE = 0.90;
 };
